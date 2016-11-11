e58cfa531f8ab4f78374c9fb64c39b5b45adfc24
ulricaskarin <ls222xp@student.lnu.se>
2016-11-11 11:29:53 +0100

    Adding Calculator and TestClass

---
+++ Calculator/Calculator/CalculatorClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorClass
+    {
+        public double Sum(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ Calculator/CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+using Xunit;
+
+namespace CalculatorTest
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void SumTest()
+        {
+            // Arrange
+            CalculatorClass sut = new CalculatorClass();
+
+            // Act
+            var actualSum = sut.Sum(2.5, 2.5);
+
+            // Assert
+            Assert.Equal(5, actualSum);
+        }
+    }
+}
